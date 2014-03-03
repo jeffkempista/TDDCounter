ec40596a7793eae95e9ec3b937051785fe0e4e4d
Jeff Kempista <jeffkempista@icloud.com>
2014-03-03 10:11:28 -0500

    Initial Commit

---
+++ TDDCounter/JKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKAppDelegate.h
+//  TDDCounter
+//
+//  Created by Jeff Kempista on 3/3/14.
+//  Copyright (c) 2014 Jeff Kempista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
